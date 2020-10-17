40650867c2132e1939bc185ba4d9e2b03e055f57
Vita <vita.harvey@seattlecolleges.edu>
2020-10-16 17:10:39 -0700

    - Doesn't crash; all buttons work and display placeholder text; - TODO: reimagine Card structure, rewrite, simplify, and adapt for current use;

--- client/src/shared/components/UIElements/OutputMap.jsx
+++ client/src/shared/components/UIElements/OutputMap.jsx
@@ -1,37 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+import GoogleMapReact from 'google-map-react';
 // import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import Map from './Map';
+import Modal from './Modal';
+import Button from '../FormElements/Button';
+import Card from './Card';
 
-import './Map.js';
-
-const center = {
-  lat: 39.833,
-  lng: -98.583
-};
+import './OutputMap.css';
 
 /**
- * InputMap is the visual interface thru which the apps takes in Reviews
- * and all of the data which will eventually be the lifeblood of the project;
- * This represents the parent-container of a Map to be used for input,
- * as well input fields for user input (eventually);
- * @param {*} props
+ * OutputMap is the view container for a Map
+ * which will be used to visualize data;
  */
 function OutputMap(props) {
 
+  // State management;
+  const [showMap, setShowMap] = useState(false);
 
-  const zoom = 8;
-  const defaultCenter = {
-    lat: 39.833, lng: -98.583
+  const openMapHandler = () => setShowMap(true);
+  const closeMapHandler = () => setShowMap(false);
+
+  props = {
+    className: "__output",
+    defaultZoom: 12,
+    defaultCenter: {
+      lat: 39.833, lng: -98.583
+    },
   };
-  const outputMap = new Map(defaultCenter);
+
+  // const defaultCenter =  {
+  //   lat : 39.833, lng : -98.583
+  // };
+
+  // const defaultZoom = 12;
 
 
   return (
-    <div>
-      {outputMap}
-    </div>
-  )
+    <React.Fragment>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="modal-content"
+        footerClass="modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <li>
+          <Card className="item__content">
+            <div id="map-container">
+              <Map center={props.defaultCenter} zoom={props.defaultZoom}/>
+            </div>
+            <div className="item__actions">
+              <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+              <Button to={`/places/${props.id}`}>EDIT</Button>
+              <Button danger>DELETE</Button>
+            </div>
+          </Card>
+        </li>
+      </Modal>
+    </React.Fragment>
+  );
 }
 
 export default OutputMap;
