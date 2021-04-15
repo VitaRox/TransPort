214d6bc475a64abe7dc6233ce0a3518d6446c021
Vita <vita.harvey@seattlecolleges.edu>
2021-04-14 17:07:08 -0700

    - Updates to use context-dependent rendering of routes UI elements (NavLinks), as well as the availability of their logical equivalents (defined as <Routes> in App.js);

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-// The basics;
-import React from 'react';
+// The basics
+import React, { useCallback, useState } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -12,38 +12,105 @@ import HomeScreen from './pages/HomeScreen';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import InputMap from './shared/components/UIElements/InputMap';
 import OutputMap from './shared/components/UIElements/OutputMap';
-
-// Stylings:
-import './App.css';
 import Login from './pages/Login';
 
+// Helpers
+import { AuthContext } from './shared/context/auth-context';
+
+// Stylings
+import './App.css';
+
+// Root-level React component, App
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <React.Fragment>
+        <Route path='/' exact>
+          <HomeScreen />
+        </Route>
+        <Route path='/data/new' exact>
+          Display a map for entering data, as well as text inputs.
+          Clicking the map creates a Google Maps Marker to be attached to the post
+          for associating the data with location.
+          <InputMap />
+        </Route>
+        <Route path='/data/view' exact>
+          Display a map component for viewing data.
+          Will include filters and a Submit button for filtering map data results.
+          <OutputMap />
+        </Route>
+        <Route path='/users/:userId'>
+          {/* This will render the user account component */}
+        </Route>
+      <Redirect to='/' />
+      </React.Fragment>
+    );
+  } else {
+    routes = (
+      <React.Fragment>
+        <Route path='/' exact>
+          <HomeScreen />
+        </Route>
+        <Route path='/data/view' exact>
+          Display a map component for viewing data.
+          Will include filters and a Submit button for filtering map data results.
+          <OutputMap />
+        </Route>
+        <Route path='/auth' exact>
+          <Login />
+        </Route>
+        <Redirect to='/auth' />
+      </React.Fragment>
+    );
+  }
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path='/' exact>
-            <HomeScreen />
-          </Route>
-          <Route path='/data/view' exact>
-            Display a map component for viewing data.
-             Will include filters and a Submit button for filtering map data results.
-            <OutputMap />
-          </Route>
-          <Route path='/data/new' exact>
-            Display a map for entering data, as well as text inputs.
-            Clicking the map creates a Google Maps Marker to be attached to the post
-            for associating the data with location.
-            <InputMap />
-          </Route>
-          <Route path='/auth' exact>
-            <Login />
-          </Route>
-          <Redirect to='/' />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route path='/' exact>
+              <HomeScreen />
+            </Route>
+            <Route path='/data/view' exact>
+              Display a map component for viewing data.
+              Will include filters and a Submit button for filtering map data results.
+              <OutputMap />
+            </Route>
+            <Route path='/data/new' exact>
+              Display a map for entering data, as well as text inputs.
+              Clicking the map creates a Google Maps Marker to be attached to the post
+              for associating the data with location.
+              <InputMap />
+            </Route>
+            <Route path='/auth' exact>
+              <Login />
+            </Route>
+            <Route path='/users/:userId'>
+              {/* This will render the user account component */}
+            </Route>
+            <Redirect to='/' />
+          </Switch>
+        </main>
+        </Router>
+      </AuthContext.Provider>
   );
 }
 
--- client/src/shared/components/Navigation/NavLinks.js
+++ client/src/shared/components/Navigation/NavLinks.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../../context/auth-context';
 
 import './NavLinks.css';
-import { NavLink } from 'react-router-dom';
+
 
 function NavLinks() {
+
+  // This component will re-render whenever the value of AuthContext is changed;
+  const auth = useContext(AuthContext);
+
   return (
     <ul className="nav-links">
       <li>
@@ -15,12 +21,16 @@ function NavLinks() {
       <li>
         <NavLink to="/data/new">VIEW INPUT MAP</NavLink>
       </li>
-      <li>
-        <NavLink to="/auth">LOG IN</NavLink>
-      </li>
-      <li>
-        <NavLink to="/users/create" exact>CREATE ACCOUNT</NavLink>
-      </li>
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">LOG IN</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/users/:userId" exact>YOUR ACCOUNT</NavLink>
+        </li>
+      )}
     </ul>
   );
 }
