1826a96ee5e091a9ec97bfd178a2e8f1232ba653
Vita <vita.harvey@seattlecolleges.edu>
2021-05-04 19:17:54 -0700

    - Fixes something dumb I misstyped in /server/models/user.js; - Reinstalls mongoose and mongodb; not sure if this somehow got overwritten by accidental push to main earlier today; should be fine now;

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema();
+const Schema = mongoose.Schema;
 const uniqueValidator = require(`mongoose-unique-validator`);
 
 // The definition of our User data entity
