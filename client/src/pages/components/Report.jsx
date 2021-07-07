23d57c8d3c457557c2d5b0efd3805aa3244dcf9e
Vita <vita.harvey@seattlecolleges.edu>
2021-07-06 19:15:37 -0700

    - Makes a Report clickable, opening a Modal which should display a detail view of the Report (full text, map focused around location, etc.); - Issue: map is covering up rest of Modal content; why?;

--- client/src/pages/components/Report.jsx
+++ client/src/pages/components/Report.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 // UI elements
 import Card from '../../shared/components/UIElements/Card';
 import Modal from "../../shared/components/UIElements/Modal";
+import Button from "../../shared/components/FormElements/Button";
+import Map from '../../shared/components/UIElements/Map';
 import './Report.css';
 
 // Represents the frontend view of a Report
@@ -30,14 +32,49 @@ const Report = props => {
   };
 
   return (
-    <Card className="report-item__">
-      <div className="report-item__info">
-        <h1>{props.imageUrl}</h1>
-        <h2>{props.title}</h2>
-        <h3>{props.address}</h3>
-        <p>{props.reportText}</p>
-      </div>
-    </Card>
+    <React.Fragment>
+      <Modal
+        show={showDetail}
+        onCancel={closeDetailHandler}
+        header={props.title}
+        contentClass="report-item__modal-content"
+        footerClass="report-item__modal-actions"
+        footer={<Button onClick={closeDetailHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <Map center={props.location} zoom={16} />
+        </div>
+      </Modal>
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass="report-item__modal-actions"
+        footer={
+          <React.Fragment>
+            <Button inverse onClick={cancelDeleteHandler}>
+              CANCEL
+            </Button>
+            <Button danger onClick={confirmDeleteHandler}>
+              PROCEED
+            </Button>
+          </React.Fragment>
+        }>
+        <p>
+          Are you quite certain you want to delete this Report?  Can't be undone.
+        </p>
+      </Modal>
+      <li className="report-item__" onClick={showDetailHandler}>
+        <Card className="report-item__content">
+          <div className="report-item__info">
+            <h1>{props.imageUrl}</h1>
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.reportText}</p>
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
   );
 
 };
