9761412bd924099bfd327a98a1e2f2ced26ff8fb
VitaRox <24485205+VitaRox@users.noreply.github.com>
2024-01-26 17:38:16 -0800

    - Adds more token-handling stuff to the front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // The basics
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -20,22 +20,53 @@ import UpdateReport from './pages/components/UpdateReport';
 // Helpers
 import { AuthContext } from './shared/context/auth-context';
 
+let logoutTimer;
+
 // Root-level React component
 const App = () => {
 
   const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
+    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({ 
+        userId: uid, 
+        token: token, 
+        expiration: tokenExpirationDate.toISOString()})
+    );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    localStorage.removeItem('userData');
   }, []);
 
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime(); // in milliseconds
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout();
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if (storedData && 
+      storedData.token && 
+      new Date(storedData.expiration) > new Date()) {
+      login(storedData.userId, storedData.token, new Date(storedData.expiration));
+    }
+  }, [login]);
+  
   let routes;
 
   if (token) {
